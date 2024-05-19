f72d92b158581dcd65b792a4025fa5d5d8933e68
Anton Medvedev <as.medvedeff96@gmail.com>
2024-05-20 00:28:30 +0500

    DRF_homeworks done

--- 3.1/products/main/views.py
+++ 3.1/products/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from main.models import Product, Review
 from main.serializers import ReviewSerializer, ProductListSerializer, ProductDetailsSerializer
 
 
@@ -11,15 +11,21 @@ def products_list_view(request):
     """реализуйте получение всех товаров из БД
     реализуйте сериализацию полученных данных
     отдайте отсериализованные данные в Response"""
-    pass
-
+    products = Product.objects.all()
+    pr_lst = ProductListSerializer(products, many=True)
+    return Response(pr_lst.data)
 
 class ProductDetailsView(APIView):
     def get(self, request, product_id):
         """реализуйте получение товара по id, если его нет, то выдайте 404
         реализуйте сериализацию полученных данных
         отдайте отсериализованные данные в Response"""
-        pass
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found'}, status=404)
+        pr_det = ProductDetailsSerializer(instance=product)
+        return Response(pr_det.data)
 
 
 # доп задание:
@@ -29,4 +35,9 @@ class ProductFilteredReviews(APIView):
         реализуйте получение отзывов по конкретному товару с определённой оценкой
         реализуйте сериализацию полученных данных
         отдайте отсериализованные данные в Response"""
-        pass
+        mark = request.GET.get('mark')
+        reviews = Review.objects.filter(product_id=product_id)
+        if mark:
+            reviews = reviews.filter(mark=mark)
+        ser = ReviewSerializer(reviews, many=True)
+        return Response(ser.data)
--- 3.1/products/products/settings.py
+++ 3.1/products/products/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'название_БД',
-        'USER': 'имя_пользователя',
-        'PASSWORD': 'пароль',
+        'NAME': 'Shop',
+        'USER': 'postgres',
+        'PASSWORD': 'Qwerty12',
         'HOST': "localhost",
         "PORT": 5432
     }
